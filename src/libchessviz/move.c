26c69e9b3e662b2f20b890666273bc75aaa0f11c
Anastasya Danko <anastasya00@example.com>
2022-05-08 20:17:14 +0700

    function for moving shapes0

---
+++ src/libchessviz/move.c
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#define BOARD_SIZE 8
+#include "libchessviz/functions.h"
+
+void move(char (*chess_board)[BOARD_SIZE],int board_size, int x1, int y1, int x2, int y2)
+{
+	chess_board[y2][x2] = chess_board[y1][x1];
+	chess_board[y1][x1] = ' ';
+
+}
