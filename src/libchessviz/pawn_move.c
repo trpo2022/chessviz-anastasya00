c0d8e73297aa4145273174f9c10a17625c6cf58e
Anastasya Danko <anastasya00@example.com>
2022-05-14 16:47:48 +0700

    Correction function

--- src/libchessviz/pawn_move.c
+++ src/libchessviz/pawn_move.c
@@ -2,17 +2,11 @@
 #include <stdlib.h>
 #define BOARD_SIZE 8
 
-int pawn_move(
-        char (*chess_board)[BOARD_SIZE],
-        int board_size,
-        int number_move,
-        int x1,
-        int y1,
-        int x2,
-        int y2)
+int pawn_move(char (*chess_board)[BOARD_SIZE], int board_size, int number_move, int x1, int y1, int x2, int y2)
 {
     if (x1 == x2 && y1 == y2) {
         printf("Вы не походили!");
+	return -1;
     }
     number_move++;
 
